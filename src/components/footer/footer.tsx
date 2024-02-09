2d948919995039e97213f770918c9802667e1283
camoesprogramming <camoesprogramming@gmail.com>
2024-02-09 19:59:47 -0300

    feat: footer and header made responsive with dropdown menu developed for mobile

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,60 +2,59 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-600 text-white">
-      <div className="container px-[171px] py-20 pt-8">
-        <div className="flex max-h-[51px] max-w-[375px] flex-col">
-          <a href="#" className="hover:text-gray-300">
-            <h2 className="text-2xl">Categorias</h2>
+    <footer className="w-full bg-gray-600 text-white">
+      <div className=" ml-9 flex flex-col pt-3">
+        <a href="#" className=" hover:text-gray-300">
+          <h2 className="text-2xl">Categorias</h2>
+        </a>
+        <div className=" flex w-2/3 flex-col justify-between md:flex-row md:flex-wrap">
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">RPG</p>
           </a>
-          <div className="flex max-w-xl flex-wrap items-center justify-between pt-3">
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">RPG</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Ação</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Aventura</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Puzzle</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Indie</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Esportes</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Corrida</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Terror</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Tiro</p>
-            </a>
-          </div>
-        </div>
-        <div className="flex max-h-[51px] max-w-[213px] flex-col py-20 pt-8">
-          <a href="#" className="hover:text-gray-300">
-            <h2 className="text-2xl">Acesso rápido</h2>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Ação</p>
           </a>
-          <div className="flex items-center justify-between pt-3">
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Novidades</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Promoções</p>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <p className="text-sm">Em breve</p>
-            </a>
-          </div>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Aventura</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Puzzle</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Indie</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Esportes</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Corrida</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Terror</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Tiro</p>
+          </a>
         </div>
       </div>
-      <p className="px-[171px] py-0 pb-8">
+      <div className="ml-9 mt-9">
+        <a href="#" className="hover:text-gray-300">
+          <h2 className="text-2xl">Acesso rápido</h2>
+        </a>
+        <div className="flex w-96 flex-col justify-between md:flex-row md:flex-wrap ">
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Novidades</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Promoções</p>
+          </a>
+          <a href="#" className="mt-3 hover:text-gray-300">
+            <p className="text-sm">Em breve</p>
+          </a>
+        </div>
+      </div>
+
+      <p className="pb-9 pt-9 text-center">
         {new Date().getFullYear()} &copy; E-PLAY Todos os direitos reservados.
       </p>
     </footer>
